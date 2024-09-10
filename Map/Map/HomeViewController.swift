81163d8874c229c5a7a0a641b7f6bb304023fb07
Mahsa <haghnevismahsa@gmail.com>
2024-09-10 21:51:35 +0330

    adde the constraints for the mapview

--- Map/Map/HomeViewController.swift
+++ Map/Map/HomeViewController.swift
@@ -19,9 +19,39 @@ class HomeViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .none
+        view.addSubview(mapView)
+        
+        setupUI()
+        
+    }
+    
+    private func setupUI(){
+        setupMap()
+    }
+    
+    private func setupMap(){
+        
+        mapView.frame = view.bounds
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor) ,
+            mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        
+        ])
+        
+        
     }
     
     
     
 }
+
+
+
