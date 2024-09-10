265b274a96a0bc63abf681c4a9e2ed6e463c5710
Mahsa <haghnevismahsa@gmail.com>
2024-09-11 01:55:58 +0330

    added a model for searchResults

---
+++ Map/Map/SearchResultModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultModel.swift
+//  Map
+//
+//  Created by Mahsa on 9/11/24.
+//
+
+struct SearchResult {
+    let title: String
+    let address: String
+    let latitude: Double
+    let longitude: Double
+}
