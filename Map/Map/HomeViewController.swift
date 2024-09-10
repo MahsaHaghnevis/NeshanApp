ff663ccd928cceb23c83aed67b2067e0fb85d5a6
Mahsa <haghnevismahsa@gmail.com>
2024-09-10 21:43:57 +0330

    modified settings for making the app programmically

---
+++ Map/Map/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Map
+//
+//  Created by Mahsa on 9/10/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
