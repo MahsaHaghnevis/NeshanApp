0ac44d26bb86a69fd8c070f1da42bbd49a6fc2ab
Mahsa <haghnevismahsa@gmail.com>
2024-09-11 16:31:15 +0330

    added the demandingfunctions to setup ui

--- Map/Map/SearchResultCellTableViewCell.swift
+++ Map/Map/SearchResultCellTableViewCell.swift
@@ -16,11 +16,29 @@ class SearchResultCellTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        setupTitleLabel()
+        setupSubtitleLabel()
+        setupiconImageView()
+    }
+    
+    private func setupTitleLabel(){
+        
+    }
+    
+    private func setupSubtitleLabel(){
+        
+    }
+    
+    private func setupiconImageView(){
+        
     }
     
 }
