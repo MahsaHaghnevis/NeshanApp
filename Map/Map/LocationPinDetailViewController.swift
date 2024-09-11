3ddeeb5b7242fd54cd535ece307b2977aa757d61
Mahsa <haghnevismahsa@gmail.com>
2024-09-11 22:38:47 +0330

    adding detail parameters for a location in LocationPinDetailViewController

---
+++ Map/Map/LocationPinDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationPinDetailViewController.swift
+//  Map
+//
+//  Created by Mahsa on 9/11/24.
+//
+
+import UIKit
+
+class LocationPinDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
