d0b0822c2bca669d3c7773a29d608de832c0b9e5
Mahsa <haghnevismahsa@gmail.com>
2024-09-11 22:55:57 +0330

    added the fucntions for setting up the configuration for parameters in LocationPinDetailViewController

--- Map/Map/LocationPinDetailViewController.swift
+++ Map/Map/LocationPinDetailViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class LocationPinDetailViewController: UIViewController {
 
+    let titleLabel = UILabel()
+    let descriptionLabel = UILabel()
+        
+     var locationTitle: String?
+     var locationDescription: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        setupUI()
+    }
+    
+    private func setupUI(){
+        
     }
     
 }
